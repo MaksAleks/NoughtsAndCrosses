abfcb39d571e22d709bc8c92e6381bb56493c352
Alekseev_M <Alekseev_M@utkonos.ru>
2021-11-24 04:10:39 +0300

    init application

---
+++ nc-app/src/main/java/ru/max/nc/ncapp/NcAppApplication.java
@@ -0,0 +1,13 @@
+package ru.max.nc.ncapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NcAppApplication.class, args);
+    }
+
+}
